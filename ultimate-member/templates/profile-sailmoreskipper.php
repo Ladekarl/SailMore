e131744bece1a1e2abd8905ae7a931a7f51baab3
nielsladekarl <nielsladekarl@gmail.com>
2020-05-26 00:04:00 +0200

    Added past events to skipper profile

--- ultimate-member/templates/profile-sailmoreskipper.php
+++ ultimate-member/templates/profile-sailmoreskipper.php
@@ -1,75 +1,75 @@
 <?php /* Template: SailMore Skipper */ ?>
-	<div class="um <?php echo $this->get_class( $mode ); ?> um-<?php echo esc_attr( $form_id ); ?> um-role-<?php echo um_user( 'role' ); ?> ">
-		<div class="um-meta-text">
-			<?php
-			sm_fetch_user_skipper();
-			$full_name = get_user_meta( um_user( 'ID' ), 'full_name', true );
-			echo esc_html( $full_name );
-			?>
-		</div>
-
-		<div class="um-form sm-profile-container">
-			<?php
-			extract( $args );
-
-			if ( !UM()->options()->get( 'profile_tab_main' ) && !isset( $_REQUEST['um_action'] ) )
-				return;
-
-			$can_view = apply_filters( 'um_profile_can_view_main', -1, um_profile_id() );
-
-			$profile = UM()->user()->profile;
-
-			if ( $can_view == -1 ) {
-				sm_profile_header_skipper( $profile );
-				sm_profile_content_main_skipper( $profile );
-			} else { ?>
-				<div class="um-profile-note"><span><i class="um-faicon-lock"></i><?php echo $can_view; ?></span></div>
-			<?php }
-			?>
-		</div>
+<div class="um <?php echo $this->get_class($mode); ?> um-<?php echo esc_attr($form_id); ?> um-role-<?php echo um_user('role'); ?> ">
+	<div class="um-meta-text">
+		<?php
+		sm_fetch_user_skipper();
+		$full_name = get_user_meta(um_user('ID'), 'full_name', true);
+		echo esc_html($full_name);
+		?>
 	</div>
 
+	<div class="um-form sm-profile-container">
+		<?php
+		extract($args);
+
+		if (!UM()->options()->get('profile_tab_main') && !isset($_REQUEST['um_action']))
+			return;
+
+		$can_view = apply_filters('um_profile_can_view_main', -1, um_profile_id());
+
+		$profile = UM()->user()->profile;
+
+		if ($can_view == -1) {
+			sm_profile_header_skipper($profile);
+			sm_profile_content_main_skipper($profile);
+		} else { ?>
+			<div class="um-profile-note"><span><i class="um-faicon-lock"></i><?php echo $can_view; ?></span></div>
+		<?php }
+		?>
+	</div>
+</div>
+
 <?php
-function sm_profile_header_skipper( $profile )
+function sm_profile_header_skipper($profile)
 {
-	?>
+?>
 
 	<div class="um-header no-cover">
 		<div class="um-profile-photo" data-user_id="<?php echo um_profile_id(); ?>">
-			<?php echo um_user( 'cover_photo' ); ?>
+			<?php echo um_user('cover_photo'); ?>
 		</div>
 		<?php
 		$uris = [];
-		if ( isset( $profile['baaden'] ) ) {
-			array_push( $uris, UM()->files()->get_download_link( 358, 'baaden', um_user( 'ID' ) ) );
+		if (isset($profile['baaden'])) {
+			array_push($uris, UM()->files()->get_download_link(358, 'baaden', um_user('ID')));
 		}
-		if ( isset( $profile['baaden_14'] ) ) {
-			array_push( $uris, UM()->files()->get_download_link( 358, 'baaden_14', um_user( 'ID' ) ) );
+		if (isset($profile['baaden_14'])) {
+			array_push($uris, UM()->files()->get_download_link(358, 'baaden_14', um_user('ID')));
 		}
-		if ( isset( $profile['baaden_15'] ) ) {
-			array_push( $uris, UM()->files()->get_download_link( 358, 'baaden_15', um_user( 'ID' ) ) );
+		if (isset($profile['baaden_15'])) {
+			array_push($uris, UM()->files()->get_download_link(358, 'baaden_15', um_user('ID')));
 		}
-		if ( isset( $profile['baaden_15_16'] ) ) {
-			array_push( $uris, UM()->files()->get_download_link( 358, 'baaden_15_16', um_user( 'ID' ) ) );
+		if (isset($profile['baaden_15_16'])) {
+			array_push($uris, UM()->files()->get_download_link(358, 'baaden_15_16', um_user('ID')));
 		}
-		if ( isset( $profile['baaden_15_16_17'] ) ) {
-			array_push( $uris, UM()->files()->get_download_link( 358, 'baaden_15_16_17', um_user( 'ID' ) ) );
+		if (isset($profile['baaden_15_16_17'])) {
+			array_push($uris, UM()->files()->get_download_link(358, 'baaden_15_16_17', um_user('ID')));
 		}
-		if ( isset( $profile['baaden_15_16_17_18'] ) ) {
-			array_push( $uris, UM()->files()->get_download_link( 358, 'baaden_15_16_17_18', um_user( 'ID' ) ) );
+		if (isset($profile['baaden_15_16_17_18'])) {
+			array_push($uris, UM()->files()->get_download_link(358, 'baaden_15_16_17_18', um_user('ID')));
 		}
 		$profUris = [];
-		if ( isset( $profile['billede_af_person_1'] ) ) {
-			array_push( $profUris, UM()->files()->get_download_link( 358, 'billede_af_person_1', um_user( 'ID' ) ) );
+		if (isset($profile['billede_af_person_1'])) {
+			array_push($profUris, UM()->files()->get_download_link(358, 'billede_af_person_1', um_user('ID')));
 		}
-		if ( isset( $profile['billede_af_person_2'] ) ) {
-			array_push( $profUris, UM()->files()->get_download_link( 358, 'billede_af_person_2', um_user( 'ID' ) ) );
+		if (isset($profile['billede_af_person_2'])) {
+			array_push($profUris, UM()->files()->get_download_link(358, 'billede_af_person_2', um_user('ID')));
 		}
-		if ( isset( $profile['billede_af_person_3'] ) ) {
-			array_push( $profUris, UM()->files()->get_download_link( 358, 'billede_af_person_3', um_user( 'ID' ) ) );
+		if (isset($profile['billede_af_person_3'])) {
+			array_push($profUris, UM()->files()->get_download_link(358, 'billede_af_person_3', um_user('ID')));
 		}
-		if ( isset( $profile['billede_af_person_4'] ) ) {
-			array_push( $profUris, UM()->files()->get_download_link( 358, 'billede_af_person_4', um_user( 'ID' ) ) );
+		if (isset($profile['billede_af_person_4'])) {
+			array_push($profUris, UM()->files()->get_download_link(358, 'billede_af_person_4', um_user('ID')));
 		}
 
 		?>
@@ -77,9 +77,9 @@ function sm_profile_header_skipper( $profile )
 		<div class="sm-profile-description">
 			<div class="sm-photo-container">
 				<?php
-				foreach ( $profUris as $proUri ) {
-					if ( isset( $proUri ) ) {
-						echo '<div class="um-photo"><a href="#" class="um-photo-modal" data-src="' . esc_attr( $proUri ) . '"><img src="' . esc_attr( $proUri ) . '" /></a></div>';
+				foreach ($profUris as $proUri) {
+					if (isset($proUri)) {
+						echo '<div class="um-photo"><a href="#" class="um-photo-modal" data-src="' . esc_attr($proUri) . '"><img src="' . esc_attr($proUri) . '" /></a></div>';
 					}
 				}
 				?>
@@ -102,73 +102,73 @@ function sm_profile_header_skipper( $profile )
 			</div>
 			<div class="sm-photo-container">
 				<?php
-				foreach ( $uris as $uri ) {
-					if ( isset( $uri ) ) {
-						echo '<div class="um-photo"><a href="#" class="um-photo-modal" data-src="' . esc_attr( $uri ) . '"><img src="' . esc_attr( $uri ) . '" /></a></div>';
+				foreach ($uris as $uri) {
+					if (isset($uri)) {
+						echo '<div class="um-photo"><a href="#" class="um-photo-modal" data-src="' . esc_attr($uri) . '"><img src="' . esc_attr($uri) . '" /></a></div>';
 					}
 				}
 				?>
 			</div>
 			<div class="sm-contact-container">
-				<a class="sm-profile-contact-button" href="<?php echo fep_query_url('newmessage', array('to' => um_user( 'ID' ) ) ); ?>">
+				<a class="sm-profile-contact-button" href="<?php echo fep_query_url('newmessage', array('to' => um_user('ID'))); ?>">
 					Kontakt mig
 				</a>
-				<a class="sm-profile-contact-button" href="/sejltogter/?skipper=<?php echo um_user('user_login')?>">
+				<a class="sm-profile-contact-button" href="/sejltogter/?skipper=<?php echo um_user('user_login') ?>&past=1">
 					Se mine togter
 				</a>
 			</div>
 		</div>
 		<div class="um-clear"></div>
 	</div>
-	<?php
+<?php
 }
 
-function sm_profile_content_main_skipper( $profile )
+function sm_profile_content_main_skipper($profile)
 {
-	?>
+?>
 	<div class="sm-profile-content sm-profile-description">
 		<?php
-		if ( isset( $profile['sejlerfaring_select'] ) )
-			add_content_skipper( 'Sejlerfaring', $profile['sejlerfaring_select'] );
-		if ( isset( $profile['certifikater'] ) )
-			add_content_skipper( 'Certifikater', extract_content_skipper( $profile['certifikater'] ) );
-		if ( isset( $profile['sejladstype'] ) )
-			add_content_skipper( 'Sejladstype', extract_content_skipper( $profile['sejladstype'] ), 'jeg vil helst sejle' );
-		if ( isset( $profile['skills'] ) )
-			add_content_skipper( 'Mine skills', extract_content_skipper( $profile['skills'] ), 'JEG KAN BIDRAGE MED' );
-		if ( isset( $profile['aktiviteter'] ) )
-			add_content_skipper( 'Aktiviteter', extract_content_skipper( $profile['aktiviteter'] ), 'jeg er glad for' );
+		if (isset($profile['sejlerfaring_select']))
+			add_content_skipper('Sejlerfaring', $profile['sejlerfaring_select']);
+		if (isset($profile['certifikater']))
+			add_content_skipper('Certifikater', extract_content_skipper($profile['certifikater']));
+		if (isset($profile['sejladstype']))
+			add_content_skipper('Sejladstype', extract_content_skipper($profile['sejladstype']), 'jeg vil helst sejle');
+		if (isset($profile['skills']))
+			add_content_skipper('Mine skills', extract_content_skipper($profile['skills']), 'JEG KAN BIDRAGE MED');
+		if (isset($profile['aktiviteter']))
+			add_content_skipper('Aktiviteter', extract_content_skipper($profile['aktiviteter']), 'jeg er glad for');
 		?>
 	</div>
-	<?php
+<?php
 }
 
-function add_content_skipper( $title, $content, $description = null )
+function add_content_skipper($title, $content, $description = null)
 {
-	?>
+?>
 	<div class="sm-profile-description-title">
 		<?php
 		echo $title
 		?>
 	</div>
-	<?php if ( isset( $description ) ) { ?>
-	<div class="sm-profile-description-text">
-		<?php
-		echo $description;
-		?>
-	</div>
-<?php } ?>
+	<?php if (isset($description)) { ?>
+		<div class="sm-profile-description-text">
+			<?php
+			echo $description;
+			?>
+		</div>
+	<?php } ?>
 	<div class="sm-profile-description-content-wrapper">
 		<?php
-		if ( is_array( $content ) ) {
-			foreach ( $content as $cont ) {
-				?>
+		if (is_array($content)) {
+			foreach ($content as $cont) {
+		?>
 				<div class="sm-profile-description-content">
 					<?php
 					echo $cont;
 					?>
 				</div>
-				<?php
+			<?php
 			}
 		} else {
 			?>
@@ -177,20 +177,20 @@ function add_content_skipper( $title, $content, $description = null )
 				echo $content;
 				?>
 			</div>
-			<?php
+		<?php
 		}
 		?>
 	</div>
-	<?php
+<?php
 }
 
-function extract_content_skipper( $arr )
+function extract_content_skipper($arr)
 {
-	$explodedArr = explode( '"', $arr );
+	$explodedArr = explode('"', $arr);
 	$contentArr = [];
-	for ( $i = 0; $i < count( $explodedArr ); $i++ ) {
-		if ( ( $i % 2 ) - 1 == 0 ) {
-			array_push( $contentArr, $explodedArr[$i] );
+	for ($i = 0; $i < count($explodedArr); $i++) {
+		if (($i % 2) - 1 == 0) {
+			array_push($contentArr, $explodedArr[$i]);
 		}
 	}
 	return $contentArr;
@@ -198,13 +198,13 @@ function extract_content_skipper( $arr )
 
 function sm_fetch_user_skipper()
 {
-	if ( um_queried_user() ) {
-		if ( UM()->options()->get( 'permalink_base' ) == 'user_login' ) {
+	if (um_queried_user()) {
+		if (UM()->options()->get('permalink_base') == 'user_login') {
 
-			$user_id = username_exists( um_queried_user() );
+			$user_id = username_exists(um_queried_user());
 			//Try
-			if ( !$user_id ) {
-				$permalink_base = UM()->options()->get( 'permalink_base' );
+			if (!$user_id) {
+				$permalink_base = UM()->options()->get('permalink_base');
 
 				// Search by Profile Slug
 				$args = array(
@@ -212,7 +212,7 @@ function sm_fetch_user_skipper()
 					'meta_query' => array(
 						array(
 							'key' => 'um_user_profile_url_slug_' . $permalink_base,
-							'value' => strtolower( um_queried_user() ),
+							'value' => strtolower(um_queried_user()),
 							'compare' => '='
 						)
 					),
@@ -220,58 +220,55 @@ function sm_fetch_user_skipper()
 				);
 
 
-				$ids = new \WP_User_Query( $args );
-				if ( $ids->total_users > 0 ) {
-					$user_id = current( $ids->get_results() );
+				$ids = new \WP_User_Query($args);
+				if ($ids->total_users > 0) {
+					$user_id = current($ids->get_results());
 				}
 			}
 
 			// Try nice name
-			if ( !$user_id ) {
+			if (!$user_id) {
 				$slug = um_queried_user();
-				$slug = str_replace( '.', '-', $slug );
-				$the_user = get_user_by( 'slug', $slug );
-				if ( isset( $the_user->ID ) ) {
+				$slug = str_replace('.', '-', $slug);
+				$the_user = get_user_by('slug', $slug);
+				if (isset($the_user->ID)) {
 					$user_id = $the_user->ID;
 				}
 
-				if ( !$user_id )
-					$user_id = UM()->user()->user_exists_by_email_as_username( um_queried_user() );
+				if (!$user_id)
+					$user_id = UM()->user()->user_exists_by_email_as_username(um_queried_user());
 
-				if ( !$user_id )
-					$user_id = UM()->user()->user_exists_by_email_as_username( $slug );
-
+				if (!$user_id)
+					$user_id = UM()->user()->user_exists_by_email_as_username($slug);
 			}
-
 		}
 
-		if ( UM()->options()->get( 'permalink_base' ) == 'user_id' ) {
-			$user_id = UM()->user()->user_exists_by_id( um_queried_user() );
+		if (UM()->options()->get('permalink_base') == 'user_id') {
+			$user_id = UM()->user()->user_exists_by_id(um_queried_user());
 		}
 
-		if ( in_array( UM()->options()->get( 'permalink_base' ), array( 'name', 'name_dash', 'name_dot', 'name_plus' ) ) ) {
-			$user_id = UM()->user()->user_exists_by_name( um_queried_user() );
+		if (in_array(UM()->options()->get('permalink_base'), array('name', 'name_dash', 'name_dot', 'name_plus'))) {
+			$user_id = UM()->user()->user_exists_by_name(um_queried_user());
 		}
 
 		/** USER EXISTS SET USER AND CONTINUE **/
 
-		if ( $user_id ) {
-			um_set_requested_user( $user_id );
-			sm_access_profile_skipper( $user_id );
+		if ($user_id) {
+			um_set_requested_user($user_id);
+			sm_access_profile_skipper($user_id);
 		} else {
-			exit( wp_redirect( um_get_core_page( 'user' ) ) );
+			exit(wp_redirect(um_get_core_page('user')));
 		}
-
 	}
 }
 
-function sm_access_profile_skipper( $user_id )
+function sm_access_profile_skipper($user_id)
 {
-	um_fetch_user( $user_id );
+	um_fetch_user($user_id);
 
-	$account_status = um_user( 'account_status' );
+	$account_status = um_user('account_status');
 
-	if ( !in_array( $account_status, array( 'approved' ) ) ) {
+	if (!in_array($account_status, array('approved'))) {
 		um_redirect_home();
 	}
 }
